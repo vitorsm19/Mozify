6dbf885b232a93bd961b2add2ead1a18fb40d985
vitorsm19 <vitormesquita190902@gmail.com>
2022-11-28 00:04:20 +0100

    Calling API on Movie Details page

--- src/Pages/Movie.jsx
+++ src/Pages/Movie.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft} from "@fortawesome/free-solid-svg-icons";
 import MovieCard from "../components/MovieCard.jsx";
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar.jsx";
 
 import "../App.css";
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
-const moviesIMG = import.meta.env.VITE_IMG;
+const moviesImgBanner = import.meta.env.VITE_IMG_BANNER;
 
 const Movie = () => {
   const { id } = useParams();
@@ -28,14 +31,37 @@ const Movie = () => {
   }, []);
 
   return (
-    <div>
+    <section
+      className="movie-page"
+      style={{
+        backgroundImage: ` linear-gradient(
+    to top,
+    rgba(5,26,55, 0.9) 45%,
+    rgba(0, 0, 0, 0)
+    ) ,url(${moviesImgBanner}${movie?.backdrop_path})`,
+      }}
+    >
+      <Navbar />
+
+      <div className="back">
+        <Link to="/">
+          <FontAwesomeIcon icon={faAngleLeft} />
+          <p>Go back</p>
+        </Link>
+      </div>
+
       {movie && (
         <>
-          <p>{movie.title}</p>
-          <p>{movie.tagline}</p>
+          {/* <p>{movie.title}</p>
+          <p>{movie.original_title}</p>
+          <p> {movie.tagline}</p>
+          <p>{movie.overview}</p>
+          <p>{movie.release_date}</p> */}
+
+          <div className="movie-banner">a</div>
         </>
       )}
-    </div>
+    </section>
   );
 };
 
